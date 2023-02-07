eff488457bcf4f1a8331d21a28118ae9bf0f44c6
Camilla Abacherly Barboza <millabacherly@gmail.com>
2023-02-07 19:00:21 -0300

    build - login de usuario e verificação do token

---
+++ src/middleware/tokenValidation.js
@@ -0,0 +1,33 @@
+const knex = require('../database/connection');
+const jwt = require('jsonwebtoken');
+const passwordJwt = require('../passwordJwt')
+
+const tokenValidation = async(req, res, next) => {
+    const { authorization } = req.headers;
+
+    if(!authorization) {
+        return res.status(401).json({ message: 'To access this resource, the valid token is required.'});
+    }
+
+    const bearerToken = authorization.slipt(' ')[1];
+
+    try {
+        const { id } = jwt.verify(bearerToken, passwordJwt);
+
+        const loggedInUser = await knex('users').where({ id }).first();
+
+        if (!loggedInUser) {
+            return res.status(404).json({ message: 'Unauthorized User'});
+        }
+
+        const { password: _, ...dataUser } = loggedInUser;
+
+        req.user = dataUser;
+
+        next();
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' + error.message });
+    }
+}
+
+module.exports = tokenValidation; 
